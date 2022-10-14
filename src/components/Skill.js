d22a028c507f216d731db502308502b5e93c445b
Taejin Kim <tj4467@naver.com>
2022-10-14 11:44:47 +0900

    skillCompoDone

--- src/components/Skill.js
+++ src/components/Skill.js
@@ -24,9 +24,9 @@ const Skill = () => {
   return (
     
         <div className='skill-page' ref={skillRef}>
-            {skillShow<600?
+            
         
-            <div>
+            <div className={(skillShow<600?"skillShow":"skill-info")}>
         <h4>SKILLS</h4>
         
         <Row> 
@@ -57,34 +57,8 @@ const Skill = () => {
         </Row>
         </div>
         
-        :
-        <Row className='invisible'> 
-            <Col md={4}>
-                <FontAwesomeIcon className='skill-icon' icon={faHtml5}/>
-                <div className='skill-name'>HTML</div>
-            </Col>
-            <Col md={4}>
-                <FontAwesomeIcon className='skill-icon' icon={faCss3Alt}/>
-                <div className='skill-name'>CSS</div>
-            </Col>           
-            <Col md={4}>
-                <FontAwesomeIcon className='skill-icon' icon={faJs}/>
-                <div className='skill-name'>JAVASCRIPT</div>
-            </Col>
-            <Col md={4}>
-                <FontAwesomeIcon className='skill-icon' icon={faBootstrap}/>
-                <div className='skill-name'>BOOTSTRAP</div>
-            </Col>
-            <Col md={4}>
-                <FontAwesomeIcon className='skill-icon' icon={faReact}/>
-                <div className='skill-name'>REACT</div>
-            </Col>
-            <Col md={4}>
-                <FontAwesomeIcon className='skill-icon' icon={faGit}/>
-                <div className='skill-name'>GIT</div>
-            </Col>
-        </Row>
-        }
+      
+        
         
     </div>
 
